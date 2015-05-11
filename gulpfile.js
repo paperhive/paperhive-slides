21e9dd48b92aa4d8d2536602d122576700358d74
André Gaul <andre@gaul.io>
2015-05-11 19:41:56 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var merge = require('merge-stream');
+
+var paths = {
+  statics: [
+    'src/index.html',
+    'src/index.css',
+    'src/templates/**/*.html',
+    'src/img/**/*'
+  ],
+  angularBootstrap: 'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+  build: 'build/**/*'
+};
+
+// copy static files
+gulp.task('statics', function () {
+  var statics = gulp.src(paths.statics, {base: 'src'})
+    .pipe(gulp.dest('build'));
+
+  var angularBootstrap = gulp.src(paths.angularBootstrap)
+    .pipe(gulp.dest('build/vendor/angular-bootstrap'));
+
+  //var fontawesome = gulp.src('bower_components/fontawesome/fonts/*')
+  //  .pipe(gulp.dest('build/assets/fontawesome/fonts'));
+
+  return merge(statics, angularBootstrap);
+});
+
+gulp.task('clean', function(cb) {
+  var del = require('del');
+
+  del(['build/*'], cb);
+});
+
+// watch for changes
+gulp.task('watch', ['default:watch'], function () {
+  gulp.watch(paths.statics, ['statics']);
+});
+
+// serve with livereload
+gulp.task('serve', ['serve:connect', 'watch', 'serve:watch']);
+
+// serve built files
+gulp.task('serve:connect', ['default:watch'], function () {
+  connect.server({
+    root: 'build',
+    livereload: true,
+  });
+});
+
+// live reload
+gulp.task('serve:reload', function () {
+  gulp.src(paths.build)
+    .pipe(connect.reload());
+});
+
+// watch built files and initiate live reload
+gulp.task('serve:watch', ['default:watch'], function () {
+  gulp.watch(paths.build, ['serve:reload']);
+});
+
+gulp.task('default', ['statics']);
+gulp.task('default:watch', ['statics']);
